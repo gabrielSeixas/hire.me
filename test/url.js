5845cd36c900198f602185877ff74d478d4f3bc3
Gabriel Seixas <gseixas13@hotmail.com>
2017-03-21 03:02:59 -0300

    Urls tests

--- test/url.js
+++ test/url.js
@@ -13,5 +13,47 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Urls', () => {
-    beforeEach((done) => {});
+    beforeEach((done) => {
+        Url.remove({}, err => done());
+    });
+
+    describe('PUT /create', () => {
+        it('should save valid urls', (done) => {
+            chai.request(server)
+                .put('/create?url=http://www.google.com')
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.alias.should.be.a('string');
+                    res.body.url.should.be.a('string');
+                    res.body.statistics.should.be.a('object');
+                    res.body.statistics.time_taken.should.be.a('string');
+                    done();
+                });
+        });
+
+        it('should not save invalid urls', (done) => {
+            chai.request(server)
+                .put('/create?url=invalid-url')
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.alias.should.eql('');
+                    res.body.err_code.should.eql('001');
+                    res.body.description.should.eql('URL IS NOT VALID');
+                    done();
+                });
+        });
+
+        it('should save urls with custom alias', (done) => {
+            chai.request(server)
+                .put('/create?url=http://www.google.com&CUSTOM_ALIAS=bemobi')
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.alias.should.eql('bemobi');
+                    res.body.url.should.contain('bemobi');
+                    res.body.statistics.should.be.a('object');
+                    res.body.statistics.time_taken.should.be.a('string');
+                    done();
+                });
+        });
+    });
 });
