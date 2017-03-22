7d0b676b90a06510b6f9bedd0ea1565f09489ec4
Gabriel Seixas <gseixas13@hotmail.com>
2017-03-22 05:30:13 -0300

    Add shortener title

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -21,7 +21,7 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to ShortenerJs</h2>
         </div>
         <p className="App-intro">
           {this.props.urls.map(this.renderUrl)}
