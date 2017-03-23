fe324b0ba87877ca1c2f01b171b7b49dd6f5c237
Gabriel Seixas <salesseixasgabriel@gmail.com>
2017-03-22 22:41:20 -0300

    Finish new modal implementation

--- client/src/components/successModal.js
+++ client/src/components/successModal.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import Modal from 'react-modal';
+import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 
 const SuccessModal = (props) => (
-  <Modal
-    isOpen={props.urls.isOpen}
-    onRequestClose={props.closeModal}
-    contentLabel="Url adicionada com sucesso">
-    <button onClick={props.closeModal}> Fechar </button>
-    <h1>Url criada</h1>
-    <a href={ props.urls.newUrl } >{ props.urls.newUrl } </a>
-  </Modal>
+  <div>
+    { props.urls.isOpen &&
+      <ModalContainer>
+        <ModalDialog onClose={props.closeModal}>
+          <h1>Url criada</h1>
+          <a href={ props.urls.newUrl } >{ props.urls.newUrl } </a>
+        </ModalDialog>
+      </ModalContainer>
+    }
+  </div>
 );
 
 export default SuccessModal;
